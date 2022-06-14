48235bc77dd9e4971409c56dd1f6684b30cdea72
AteevDuggal <akd3257@gmail.com>
2022-06-14 13:54:29 +0530

    Name updated

--- src/Hero.js
+++ src/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
             className="col-12 display-5 py-2 px-4 text-white"
             style={{ backgroundColor: "rgb(8, 44, 87)" }}
           >
-            Tekolio
+            Pharma +
           </div>
           <div
             id="carouselExampleIndicators"
